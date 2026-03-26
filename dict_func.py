4e24143912f07aa1c67ae4d988ecbea8e2ceec18
Julian Quintero <julianqula@unisabana.edu.co>
2026-03-26 11:11:51 -0500

    lab6-5

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,14 @@
-# Write your code here!
+def employee_print(employee_info):
+    name = employee_info.get("nombre","N/A")
+    salary = employee_info.get("salario","N/A")
+    role = employee_info.get("rol","N/A")
+    print(f"Name: {name}\nSalary: {salary}\nRole: {role}\n")
+    remaining = employee_info.copy()
+    remaining.pop("nombre", None)
+    remaining.pop("salario", None)
+    remaining.pop("rol", None)
+    if remaining:
+        for key, value in remaining.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
