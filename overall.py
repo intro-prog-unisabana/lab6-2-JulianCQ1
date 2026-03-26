229f4596358de6565eece796f930173ac4d3c4e7
Julian Quintero <julianqula@unisabana.edu.co>
2026-03-26 11:52:03 -0500

    lab6-11

--- overall.py
+++ overall.py
@@ -1,21 +1,21 @@
 def student_averages(estudiantes):
     promedios = {}
     for estudiante, notas in estudiantes.items():
-        average = sum(notas.values()) / len(notas)
+        average = round(sum(notas.values()) / len(notas))
         promedios[estudiante] = average
     return promedios
 def assignment_averages(estudiantes):
     total = {}
     cuenta = {}
-    for asignatura in estudiantes.values():
-        for materia, nota in asignatura.items():
-            if asignatura not in total:
-                total[asignatura] = 0
-                cuenta[asignatura] = 0
+    for notas in estudiantes.values():
+        for materia, nota in notas.items():
+            if materia not in total:
+                total[materia] = 0
+                cuenta[materia] = 0
             total[materia] = total.get(materia, 0) + nota
             cuenta[materia] = cuenta.get(materia, 0) + 1
     resultado = {}
     for asignatura in total:
         resultado[asignatura] = total[asignatura] / cuenta[asignatura]
-        return resultado
+    return resultado
    
