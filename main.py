ee75fbb4d7e3ca30d590691fff9313bdf989ff0c
Julian Quintero <julianqula@unisabana.edu.co>
2026-03-26 16:37:15 -0500

    lab6-15

---
+++ main.py
@@ -0,0 +1,44 @@
+from grades_manager import *
+
+def main():
+    print("Welcome to the Student Grades Manager!")
+    my_grades = {}
+
+    while True:
+        print("\nSelect an option:")
+        print("1. Add a student")
+        
+      
+        option = input()
+
+        if option == "1":
+            my_grades = add_student(my_grades)
+
+        elif option == "2":
+            print("Select an option:")
+            print("a. Display all students")
+            print("b. Display selected students")
+
+            sub = input()
+
+            if sub == "a":
+                avg_by_student(my_grades)
+
+            elif sub == "b":
+                names = input("Enter student names (comma-separated):\n")
+                keys = [n.strip() for n in names.split(",")]
+                avg_by_student(my_grades, keys)
+
+            else:
+                print("Invalid option selected!")
+
+        elif option == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option selected!")
+
+
+if __name__ == "__main__":
+    main()
